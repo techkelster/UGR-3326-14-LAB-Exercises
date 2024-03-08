0a19820ef3910f7721b1388ef00b667c0722e1fd
techkelster <techkelster@gmail.com>
2024-03-08 14:47:09 +0300

    LAB-1-variables-and-data-types.dart

---
+++ LAB-1-variables-and-data-types.dart
@@ -0,0 +1,8 @@
+void main() {
+  String name = "Abdulmejd";
+  int age = 22;
+  String favoriteColor = "Brown";
+
+  print(
+      "My name is $name. I am $age years old, and my favorite color is $favoriteColor.");
+}
